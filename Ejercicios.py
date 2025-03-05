a72b466ee6fdad8d254a915af29dd78cba45d704
Erika <up240406@alumnos.upa.edu.mx>
2025-03-05 10:30:06 -0600

    3

--- Ejercicios.py
+++ Ejercicios.py
@@ -1,6 +1,6 @@
 #Dia 3
 
-#Declare your age as integer variable
+#1 y 2Declare your age as integer variable
 age = 18 
 hight = 1.65
 numcom = 8j
@@ -8,34 +8,34 @@ print (type(age), age)
 print (type(hight), hight)
 print (type(numcom), numcom)
 
-# Write a script that prompts the user to enter base and height 
+# 3 Write a script that prompts the user to enter base and height 
 # of the trianngle and calculate an area of this triangle
 # (area = 0.5 x b x h). 
 base = float(input ("Ingresa el valor de la base: "))
 altura = float(input("Ingresa el valor de la altura: "))
 area = print ("El area del triangulo es: ", base*altura/2)
 
-# Write a script that prompts the user to enter side a, side b, and side c of the triangle. 
+# 4 Write a script that prompts the user to enter side a, side b, and side c of the triangle. 
 #  Calculate the perimeter of the triangle (perimeter = a + b + c).
 sideA = float (input("Ingresar el valor de sideA")) 
 sideB = float (input("Ingresar el valor de sideB"))
 sideC = float (input('Ingresar el valor de sideC'))
 perimeter= print ("El valor del perimetro del triangulo es:", sideA + sideB + sideC )
 
-#Get length and width of a rectangle using prompt.
+# 5 Get length and width of a rectangle using prompt.
 #  Calculate its area (area = length x width) and perimeter (perimeter = 2 x (length + width))
 largo = float (input("ingresa el largo del rectangulo"))
 ancho = float(input("ingresa el ancho del triangulo"))
 Area = print("El area del triangulo es de:", largo * ancho)
 perimeter = print("El perimetro del triangulo es de:",2 * (largo + ancho))
 
-#Get radius of a circle using prompt.
+# 6 Get radius of a circle using prompt.
 #  Calculate the area (area = pi x r x r) and circumference (c = 2 x pi x r) where pi = 3.14.
 pi = 3.14
 r = float (input("ingresa el valor del radio"))
 Area = print("El area del circulo es de :", pi * r * r )
 circunferencia = print("La circunferencia del circulo es de :", 2 * pi * r)
-#Calculate the slope, x-intercept and y-intercept of y = 2x -2
+# 7 Calculate the slope, x-intercept and y-intercept of y = 2x -2
 pendiente1 = 2
 interceptY = -2
 interceptX = interceptY / pendiente1
@@ -43,7 +43,7 @@ print("la pendiente de la recta es de :", pendiente1)
 print("La interseccion en el eje Y es:", interceptY)
 print(" La interseccion en el eje X es:", interceptX)
 
-#Slope is (m = y2-y1/x2-x1).
+# 8 Slope is (m = y2-y1/x2-x1).
 # Find the slope and Euclidean distance between point (2, 2) and point (6,10)
 point1 = (2, 2)
 point2 = (6, 10)
@@ -56,7 +56,7 @@ Distancia = ((X2 - X1)** 2 + (Y2 - Y1))** 0.5
 print ("La pendiente de los puntos es:", pendiente2)
 print ("La distancia entre los puntos es:", Distancia)
  
-##Calculate the slope, x-intercept and y-intercept of y = 2x -2
+# 9 Calculate the slope, x-intercept and y-intercept of y = 2x -2
 pendiente = 2
 interceptY = -2
 interceptX = interceptY / pendiente
@@ -64,14 +64,13 @@ print("la pendiente de la recta es de :", pendiente)
 print("La interseccion en el eje Y es:", interceptY)
 print(" La interseccion en el eje X es:", interceptX)
 
-#Compare the slopes in tasks 8 and 9.
+# 10 Compare the slopes in tasks 8 and 9.
 print ("La pendiente 1 es de:",pendiente1)
 print ("La pendiente 2 es de :",pendiente2)
 pendiente1 < pendiente2
-if True:
-    print ("La pendiente 1 es menor que la pendiente 2")
+if  print ("La pendiente 1 es menor que la pendiente 2")
     
-#Calculate the value of y (y = x^2 + 6x + 9).
+# 11 Calculate the value of y (y = x^2 + 6x + 9).
 #Try to use different x values and figure out at what x value y is going to be 0.
 # Definir la función
 x= int(input("Ingresar el valor de x:" ))
@@ -82,7 +81,57 @@ if y==0:
 else: 
     print ("y no es igual a 0.")
 
-#Find the length of 'python' and 'dragon' and make a falsy comparison statement.
+# 12 Find the length of 'python' and 'dragon' and make a falsy comparison statement.
+longitud_python = len("python")
+longitud_dragon = len("dragon")
+print("longitud de python", longitud_python)
+print("longitud de dragon", longitud_dragon)
+#comparacion falsa
+print("la longitud de pytho no es igual a la longitud de dragon", longitud_python!=longitud_dragon)
+
+# 13 Use and operator to check if 'on' is found in both 'python' and 'dragon
+frase1 = "python"
+frase2 = "dragon"
+resultado = ("on" in frase1 and "on" frase2)
+print(resultado)
+
+# 14 I hope this course is not full of jargon. Use in operator to check if jargon is in the sentence.
+palabra = "jargon"
+resultado = ("jargon", in palabra)
+print( resultado)
+# 15 There is no 'on' in both dragon and python
+frase1 = "pyhton"
+frase2 = "dragon"
+resultado = ("no "on" " in frase1 and "no "on"")
+             
 
 
-#Use and operator to check if 'on' is found in both 'python' and 'dragon
+# 16 Find the length of the text python and convert the value to float and convert it to string
+
+
+
+
+
+# 17 Even numbers are divisible by 2 and the remainder is zero. How do you check if a number is even or not using python?
+
+
+
+
+# 18 Check if the floor division of 7 by 3 is equal to the int converted value of 2.7.
+
+
+
+
+# 19 Check if type of '10' is equal to type of 10
+
+
+
+
+# 20 Check if int('9.8') is equal to 10
+
+
+
+
+
+
+# 21 Writ a script that prompts the user to enter hours and rate per hour. Calculate pay of the person?
