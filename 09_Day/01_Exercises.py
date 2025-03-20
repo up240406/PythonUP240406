3deb59aff4ef25d9818c10409040d7153a3ac591
Erika <up240406@alumnos.upa.edu.mx>
2025-03-20 11:48:55 -0600

    dia 9

---
+++ 09_Day/01_Exercises.py
@@ -0,0 +1,8 @@
+#1 Get user input using input(“Enter your age: ”). If user is 18 or older, give feedback: 
+# You are old enough to drive. If below 18 give feedback to wait for the missing amount of years. 
+# Output:
+Edad = int(input("Ingresa tu edad"))
+if Edad >= 18:
+    print('Eres lo suficientemente viejo para conducire')
+else:
+    print
